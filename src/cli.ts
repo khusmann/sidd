d4bbbf2d750c92f69c19b233c28e5c94066f6736
Kyle Husmann <kyle.husmann@gmail.com>
2023-11-03 11:32:21 -0700

    experiment with bundling from cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,36 @@
 #!/usr/bin/env node
 
+import { Parcel } from "@parcel/core";
+import ThrowableDiagnostic from "@parcel/diagnostic";
+
 import { generateDataPackage } from "./synth/generator";
 
 const synthDataPackage = generateDataPackage();
 
 console.log(JSON.stringify(synthDataPackage, null, 2));
+
+const bundler = new Parcel({
+  entries: "./src/codebook.html",
+  defaultConfig: "@parcel/config-default",
+  mode: "production",
+  serveOptions: {
+    port: 3000,
+  },
+});
+
+const main = async () => {
+  // await bundler.watch();
+  try {
+    const { bundleGraph, buildTime } = await bundler.run();
+    const bundles = bundleGraph.getBundles();
+    console.log(`✨ Built ${bundles.length} bundles in ${buildTime}ms!`);
+  } catch (err) {
+    if (err instanceof ThrowableDiagnostic) {
+      console.log(err.diagnostics);
+    } else {
+      console.log(err);
+    }
+  }
+};
+
+void main();
